4d070c5bced9d719342b20f694967b1fdc1dc218
MarikPet <marikpetros@gmail.com>
2026-04-13 16:28:15 +0400

    environment and file arrangement

--- init/03_elt_raw_coffee_shop.sql
+++ init/03_elt_raw_coffee_shop.sql
@@ -1,5 +1,5 @@
 --- 3. Load data into the raw table
 COPY analytics.coffee_shop_raw
-FROM '/docker-entrypoint-initdb.d/data/coffee_shop/coffee-shop-sales-revenue.csv'
+FROM '/docker-entrypoint-initdb.d/data/analytics_schema/coffee_shop/coffee-shop-sales-revenue.csv'
 CSV HEADER DELIMITER '|' 
 NULL 'NULL';
